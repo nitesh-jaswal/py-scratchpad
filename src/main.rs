4c44e6077abdb9761cef0dc8ce8de87cc9ca0172
Nitesh Singh Jaswal <nitesh.jaswal@signify.com>
2023-01-27 23:34:28 +0100

    Added a function command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,109 @@
-use lexopt::*;
-use lexopt::prelude::*;
-use std::path::{Path, PathBuf};
-use std::env;
-use rand::seq::SliceRandom;
-// TODO:
-// * create new file in temp
-// * open it in code editor
-// * cleanup on close
-// * 
-
-fn get_random_four_character_id() -> String {
-    let base_string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".as_bytes();
-    let id = base_string.choose_multiple(&mut rand::thread_rng(), 4).map(|x| -> u8 {*x}).collect::<Vec<_>>();
-    String::from_utf8_lossy(id.as_slice()).to_string()
-    // base_string
-}
-
+use lexopt::{Parser, Arg::*};
+use std::{path::{Path, PathBuf}, os};
+use std::ffi::OsString;
 
 #[derive(Debug)]
-struct Scratchpad {
+enum CLICommands {
+    Help,
+    Config {
         editor: PathBuf,
-        venv: PathBuf,
-        tmp_dir: PathBuf,
-}
-
-impl Scratchpad {
-    fn parse_args() -> Self {
-        use lexopt::prelude::*;
-
-        let home = env::var("HOME").unwrap();
-        let home = Path::new(home.as_str());
-        let editor = PathBuf::from("/usr/local/bin/code");
-        let venv = home.join(Path::new(".scratchpad/envs/DEFAULT_ENV/"));
-        let tmp_dir = home.join(Path::new(".scratchpad/tmp"));
-        let mut parser = lexopt::Parser::from_env();
-        while let Some(arg) = parser.next().expect("Unhandled error!") {
-            match arg {
-                Short('n') | Long("name") => {
-                    let val= parser.value().unwrap();
-                    println!("name!! {}", val.to_str().unwrap());
-                },
-                Short('h') | Long("help") => {
-                    println!("Usage: hello [-n|--number=NUM] [--shout] THING");
-                    std::process::exit(0);
-                },
-                _ => panic!("Unexpected argument encountered {:?}", arg)
-            };
-        }
-        Self {
-            editor: editor,
-            venv: venv,
-            tmp_dir: tmp_dir
-        }
+        workspace: PathBuf,
+        python_path: PathBuf,
+        active_venv: PathBuf,
+        last_created_file: Option<PathBuf>
+    },
+    New,
+    List,
+    Run {
+        partial_id: Option<String>
+    },
+    Delete {
+        partial_id: String
+    },
+    Open {
+        partial_id: Option<String>
     }
 }
-    
-fn main() {
-    // let id = get_random_four_character_id();
-    let args = Scratchpad::parse_args();
-    println!("{:?}", args);
+
+fn parse_args() -> Result<CLICommands, lexopt::Error> {
+    let mut parser = lexopt::Parser::from_env();
+    let cli_arg = parser.next()?;
+    if let Some(some_cmd) = cli_arg {
+        let cmd = match some_cmd {
+            Value(val) => { 
+                match val.to_str() {
+                    Some("help") => { CLICommands::Help },
+                    Some("new") => { CLICommands::New },
+                    Some("list") => { CLICommands::List },
+                    Some("run") => { 
+                        match parser.value() {
+                            Ok(option) => {
+                                if let Some(id) = option.to_str() {
+                                    CLICommands::Run { partial_id: Some(String::from(id)) }
+                                }
+                                else {
+                                    return Err(lexopt::Error::NonUnicodeValue(option))
+                                }
+                            },
+                            Err(lexopt::Error::MissingValue { option: _ }) => { CLICommands::Run { partial_id: None } },
+                            Err(err) => { return Err(err) } 
+                        }
+                    },
+                    Some("delete") => { 
+                        match parser.value() {
+                            Ok(option) => {
+                                if let Some(id) = option.to_str() {
+                                    CLICommands::Delete{ partial_id: String::from(id) }
+                                }
+                                else {
+                                    return Err(lexopt::Error::NonUnicodeValue(option))
+                                }
+                            },
+                            Err(err) => { return Err(err) } 
+                        }
+                    },
+                    Some("open") => { 
+                        match parser.value() {
+                            Ok(option) => {
+                                if let Some(id) = option.to_str() {
+                                    CLICommands::Open { partial_id: Some(String::from(id)) }
+                                }
+                                else {
+                                    return Err(lexopt::Error::NonUnicodeValue(option))
+                                }
+                            },
+                            Err(lexopt::Error::MissingValue { option: _ }) => { CLICommands::Open { partial_id: None } },
+                            Err(err) => { return Err(err) } 
+                        }
+                    },
+                    Some("config") => {
+                        println!("ToDo. Menu will be walked through here");
+                        CLICommands::Help
+                    },
+                    Some(_) | None => {
+                        println!("Invalid usage! Please refer below for correct usage");
+                        CLICommands::Help
+                    }
+                }
+            },
+            _ => {
+                println!("Invalid usage! Please refer below for correct usage");
+                CLICommands::Help
+            }
+        };
+        return Ok(cmd)
+    }
+    else {
+        println!("Invalid usage! Please refer below for correct usage");
+        return Ok(CLICommands::Help)
+    }
+}
+
+// Print cli menu for configuration. Define default behaviour when starting first time
+// Write individual functionality for virtual env creation, temp_dir mgmt and editor
+// Write 
+fn main () -> Result<(), lexopt::Error> {
+    let cli_commands =  parse_args()?;
+    println!("Parsed command: {:?}", cli_commands);
+    Ok(())
 }
